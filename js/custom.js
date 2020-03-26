2b0f0ec5dfd18e4539ab3f6b4cf6d2a87c28d87d
utshaweu <mahmudsajib95@gmail.com>
2020-03-26 18:59:43 +0600

    Smooth Scroll js

--- js/custom.js
+++ js/custom.js
@@ -26,6 +26,22 @@ $(function(){
         html_body.animate({scrollTop:0},1000);
     });
     
+    
+    //Smooth Scroll js
+    var html_body = $('html, body');
+    $('nav a').on('click', function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                html_body.animate({
+                    scrollTop: target.offset().top - 0
+                }, 1500);
+                return false;
+            }
+        }
+    });	
+    
     //Venobox
     $('.venobox').venobox();
     
